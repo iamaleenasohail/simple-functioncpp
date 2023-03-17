ec9022fd7651eede979bfd802d1ce2bd7aa7d0b5
AleenaSohail <128075228+iamaleenasohail@users.noreply.github.com>
2023-03-17 07:33:37 -0700

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void line();
+int main()
+{
+	cout<<"hello";
+	line();
+	cout<<"i am aleena";
+	line();
+	cout<<"i am student";
+	line();
+	cout<<"i am from kahsmir";
+}
+void line()
+{
+	cout<<"==========";
+	cout<<"==========";
+}
+	
